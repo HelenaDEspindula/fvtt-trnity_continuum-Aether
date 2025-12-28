4994a5209ec1c376c09e1c4729e5bdd2d144fb61
Helena R. S. D'Espindula <58571971+HelenaDEspindula@users.noreply.github.com>
2025-12-27 21:00:01 -0300

    Refactor Aether initialization logging and registration

--- module/system.js
+++ module/system.js
@@ -1,13 +1,19 @@
+import { AETHER } from "./constants.js";
 import { AetherActor } from "./actor/actor.js";
 import { AetherActorSheet } from "./actor/actor-sheet.js";
 
 Hooks.once("init", () => {
-  console.log("Aether | init");
+  console.log(`${AETHER.ID} | init`);
+
+  CONFIG.AETHER = AETHER;
 
   CONFIG.Actor.documentClass = AetherActor;
 
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("fvtt-trnity_continuum-Aether", AetherActorSheet, {
-    makeDefault: true
-  });
+  Actors.registerSheet(AETHER.ID, AetherActorSheet, { makeDefault: true });
+
+  loadTemplates([
+    `systems/${AETHER.ID}/templates/actor/character-sheet.hbs`,
+    `systems/${AETHER.ID}/templates/actor/npc-sheet.hbs`
+  ]);
 });
