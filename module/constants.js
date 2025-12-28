9ef3a7bb958a4f035f59791263e4417f07c3589e
Helena R. S. D'Espindula <58571971+HelenaDEspindula@users.noreply.github.com>
2025-12-27 21:00:31 -0300

    Add AETHER constants with attributes, skills, and facets

---
+++ module/constants.js
@@ -0,0 +1,40 @@
+export const AETHER = {
+  ID: "fvtt-trnity_continuum-Aether",
+
+  ATTRIBUTES: {
+    might: "Might",
+    dexterity: "Dexterity",
+    stamina: "Stamina",
+    intellect: "Intellect",
+    cunning: "Cunning",
+    resolve: "Resolve",
+    presence: "Presence",
+    manipulation: "Manipulation",
+    composure: "Composure"
+  },
+
+  SKILLS: {
+    aim: "Aim",
+    athletics: "Athletics",
+    closeCombat: "Close Combat",
+    command: "Command",
+    culture: "Culture",
+    empathy: "Empathy",
+    enigmas: "Enigmas",
+    humanities: "Humanities",
+    integrity: "Integrity",
+    larceny: "Larceny",
+    medicine: "Medicine",
+    persuasion: "Persuasion",
+    pilot: "Pilot",
+    science: "Science",
+    survival: "Survival",
+    technology: "Technology"
+  },
+
+  FACETS: {
+    intuitive: "Intuitive",
+    reflective: "Reflective",
+    destruction: "Destruction"
+  }
+};
